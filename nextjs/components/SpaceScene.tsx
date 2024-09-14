e88f60e48cd15255e63757242ab208cfcb875f5d
lollo-santucci <lorenz.santucci@gmail.com>
2024-09-14 14:27:42 +0000

    import cursor components

--- nextjs/components/SpaceScene.tsx
+++ nextjs/components/SpaceScene.tsx
@@ -2,6 +2,7 @@
 
 import { Canvas } from '@react-three/fiber'
 import { Stars } from "@/components/Stars"
+import Cursor3D from '@/components/Cursor3D'
 import { OrbitControls } from '@react-three/drei';
 
 export function SpaceScene() {
@@ -9,6 +10,7 @@ export function SpaceScene() {
     <div style={{ width: '100vw', height: '100vh', background: '#1A3546' }}>
       <Canvas camera={{ position: [0, 0, 10], fov: 60 }}>
         <Stars/>
+        <Cursor3D/>
         {/*<OrbitControls/>*/}
       </Canvas>
     </div>
