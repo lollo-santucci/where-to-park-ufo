f1fd0590bd77ebed8ba5c979d7d68c9fc0bb37af
lollo-santucci <lorenz.santucci@gmail.com>
2024-09-15 19:44:33 +0000

    change background color

--- nextjs/components/SpaceScene.tsx
+++ nextjs/components/SpaceScene.tsx
@@ -2,12 +2,13 @@
 
 import { Canvas } from '@react-three/fiber'
 import { Stars } from "@/components/Stars"
+
 import Cursor3D from '@/components/Cursor3D'
 import { OrbitControls } from '@react-three/drei';
 
 export function SpaceScene() {
   return (
-    <div style={{ width: '100vw', height: '100vh', background: '#1A3546' }}>
+    <div style={{ width: '100vw', height: '100vh', background: '#1F3847' }}>
       <Canvas camera={{ position: [0, 0, 10], fov: 60 }}>
         <Stars/>
         <Cursor3D/>
